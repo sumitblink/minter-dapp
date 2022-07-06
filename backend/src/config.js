0c3be9766c3faa088ee94ade1e78f7ab1d797188
sumitblink <sumit@blinkdigital.in>
2022-07-06 13:05:25 +0530

    test

--- backend/src/config.js
+++ backend/src/config.js
@@ -53,7 +53,7 @@ const OWNER_ADDRESS = '0x967274381065F080a90181b1540FB880233A0953';
 const TREASURY_ADDRESS = '0x967274381065F080a90181b1540FB880233A0953';
 const MAX_SUPPLY = 20; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 2; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 2; // maximum number of NFTs a user can mint in a single transction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-07-15T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00 //YYYY-MM-DD
